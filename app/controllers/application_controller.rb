5515752ffcc430a68f581f5dc8153908701b4bce
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-06-14 16:36:39 +0900

    各種CSSの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth
+  
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :birthday])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
@@ -10,7 +14,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :birthday])
-  end
 end
