13603c376774fdaa8fc6533d1d15f6765510296d
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-06-14 19:40:16 +0900

    テストコードの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {'test'}
+    nickname { 'テスト' }
     email {Faker::Internet.free_email}
     password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    first_name { '太郎' }
+    last_name { '山田' }
+    first_name_reading { 'タロウ' }
+    last_name_reading { 'ヤマダ' }
+    birthday { '1940-01-01' }
   end
 end
