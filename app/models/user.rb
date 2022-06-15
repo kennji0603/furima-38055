640ee8eb987d72d05733756e5872d6f0d76503ca
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-06-15 15:54:20 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         
+              
   with_options presence: true do
-    validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :first_name_reading,
+    validates :nickname, :birthday, :password_confirmation
+    validates :first_name, :last_name, 
+               format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :first_name_reading, :last_name_reading,
               format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :last_name_reading,
-              format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :birthday
   end
-end
+  validates :password,  :password_confirmation,
+          format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Input half-width characters.'}
+        
+end  
