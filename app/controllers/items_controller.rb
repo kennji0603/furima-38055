0309b0e8be870d0cc87b33fc59c480dfa8bb764f
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-06-17 09:18:56 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit]
+
   def index
     @items = Item.order('created_at DESC')
   end
@@ -18,7 +20,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
+  end
+
+  def edit
   end
 
   private
@@ -26,5 +31,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :explanation, :price, :image, :category_id, :condition_id, :postagetype_id,
                                  :prefecture_id, :preparationday_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
