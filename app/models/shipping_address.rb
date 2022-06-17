a5492b192fd22d2be27187f3ecbd1b7ab1a8d2c7
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-06-17 17:41:58 +0900

    Formオブジェクトの作成

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -3,18 +3,14 @@ class ShippingAddress < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
  
-  with_options presence: true do
-    validates :city, :house_number
-  end
+  #with_options presence: true do
+    #validates :city, :house_number
+    #validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    #validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input half-width characters.' }
+  #end
 
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  #validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
-  with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Input half-width characters.' }
-  end
-
-  with_options presence: true do
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input half-width characters.' }
-  end
+  
 end
 
