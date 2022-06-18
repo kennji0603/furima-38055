2a85eca854c53be8b03729780df4c8ef131efada
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-06-18 14:37:12 +0900

    コントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,21 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+      @order_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping_address).permit(:postal_code, :city, :house_number, :building_name, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
