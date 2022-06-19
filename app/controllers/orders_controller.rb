b837dc3d2cade02c51dd60d949dd8ea5b37ae525
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-06-19 15:45:08 +0900

    rubocop実行

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -21,22 +21,22 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping_address).permit(:postal_code, :city, :house_number, :building_name, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+    params.require(:order_shipping_address).permit(:postal_code, :city, :house_number, :building_name, :phone_number, :prefecture_id).merge(
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
+    )
   end
 
   def move_to_items_index
     @item = Item.find(params[:item_id])
-   unless  @item.order.blank? && @item.user_id != current_user.id
-    redirect_to root_path
-   end
+    redirect_to root_path unless @item.order.blank? && @item.user_id != current_user.id
   end
 
   def pay_item
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
-  end   
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- spec/models/order_shipping_address_spec.rb
+++ spec/models/order_shipping_address_spec.rb
@@ -9,9 +9,8 @@ RSpec.describe OrderShippingAddress, type: :model do
       @item.save
       sleep 0.1
       @order_shipping_address = FactoryBot.build(:order_shipping_address, user_id: @user.id, item_id: @item.id)
-      
     end
-       
+
     context '内容に問題ない場合' do
       it 'すべての値が正しく入力されていれば保存できること' do
         expect(@order_shipping_address).to be_valid
@@ -22,25 +21,25 @@ RSpec.describe OrderShippingAddress, type: :model do
         expect(@order_shipping_address).to be_valid
       end
     end
-      
+
     context '内容に問題がある場合' do
       it 'postal_codeが空だと保存できないこと' do
         @order_shipping_address.postal_code = ''
         @order_shipping_address.valid?
-        expect(@order_shipping_address.errors.full_messages).to include "Postal code can't be blank", "Postal code is invalid. Include hyphen(-)"
+        expect(@order_shipping_address.errors.full_messages).to include "Postal code can't be blank",
+                                                                        'Postal code is invalid. Include hyphen(-)'
       end
 
       it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
         @order_shipping_address.postal_code = '1234567'
         @order_shipping_address.valid?
-        expect(@order_shipping_address.errors.full_messages).to include "Postal code is invalid. Include hyphen(-)"
+        expect(@order_shipping_address.errors.full_messages).to include 'Postal code is invalid. Include hyphen(-)'
       end
 
       it 'prefecture_idを選択していないと保存できないこと' do
         @order_shipping_address.prefecture_id = 1
         @order_shipping_address.valid?
         expect(@order_shipping_address.errors.full_messages).to include "Prefecture can't be blank"
-      
       end
 
       it 'cityが空だと保存できないこと' do
@@ -59,31 +58,31 @@ RSpec.describe OrderShippingAddress, type: :model do
         @order_shipping_address.phone_number = ''
         @order_shipping_address.valid?
         expect(@order_shipping_address.errors.full_messages).to include "Phone number can't be blank",
-        "Phone number is invalid. Input half-width characters."
+                                                                        'Phone number is invalid. Input half-width characters.'
       end
 
       it 'phone_numberが10桁未満だと保存できない' do
         @order_shipping_address.phone_number = '090111111'
         @order_shipping_address.valid?
-        expect(@order_shipping_address.errors.full_messages).to include "Phone number is invalid. Input half-width characters."
+        expect(@order_shipping_address.errors.full_messages).to include 'Phone number is invalid. Input half-width characters.'
       end
 
       it 'phone_numberが11桁を超えていると保存できない' do
-         @order_shipping_address.phone_number = '090111111111'
-         @order_shipping_address.valid?
-         expect(@order_shipping_address.errors.full_messages).to include "Phone number is invalid. Input half-width characters."
+        @order_shipping_address.phone_number = '090111111111'
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include 'Phone number is invalid. Input half-width characters.'
       end
 
       it 'phone_numberが全角数字だと保存できない' do
         @order_shipping_address.phone_number = '０９０１１１１１１１１'
-         @order_shipping_address.valid?
-         expect(@order_shipping_address.errors.full_messages).to include "Phone number is invalid. Input half-width characters."
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include 'Phone number is invalid. Input half-width characters.'
       end
 
       it 'phone_numberがハイフンを含んだ形だと保存できない' do
-         @order_shipping_address.phone_number = '090-1111-1111'
-         @order_shipping_address.valid?
-         expect(@order_shipping_address.errors.full_messages).to include "Phone number is invalid. Input half-width characters."
+        @order_shipping_address.phone_number = '090-1111-1111'
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include 'Phone number is invalid. Input half-width characters.'
       end
 
       it 'userが紐ついてないと保存できない' do
@@ -98,12 +97,11 @@ RSpec.describe OrderShippingAddress, type: :model do
         expect(@order_shipping_address.errors.full_messages).to include "Item can't be blank"
       end
 
-      it "tokenが空では保存できないこと" do
+      it 'tokenが空では保存できないこと' do
         @order_shipping_address.token = nil
         @order_shipping_address.valid?
         expect(@order_shipping_address.errors.full_messages).to include("Token can't be blank")
       end
     end
   end
-    
 end
