509390e3b443e8005767eb607fbb0e8d4b4024a4
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-06-20 09:00:51 +0900

    リファクタリングの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,12 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :move_to_items_index, only: [:index, :create]
+  before_action :item_params, only: [:index, :create]
   def index
-    @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new(order_params)
     if @order_shipping_address.valid?
       pay_item
@@ -20,6 +19,10 @@ class OrdersController < ApplicationController
 
   private
 
+  def item_params
+    @item = Item.find(params[:item_id])
+  end
+
   def order_params
     params.require(:order_shipping_address).permit(:postal_code, :city, :house_number, :building_name, :phone_number, :prefecture_id).merge(
       user_id: current_user.id, item_id: @item.id, token: params[:token]
@@ -27,7 +30,7 @@ class OrdersController < ApplicationController
   end
 
   def move_to_items_index
-    @item = Item.find(params[:item_id])
+    item_params
     redirect_to root_path unless @item.order.blank? && @item.user_id != current_user.id
   end
 
