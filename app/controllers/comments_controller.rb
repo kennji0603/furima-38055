518388988acb395d00d1bd0ff7afb0f142442f58
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-06-22 16:27:35 +0900

    コントローラの編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+
+  def create
+    binding.pry
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user} 
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
